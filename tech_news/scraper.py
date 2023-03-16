06975a5546f4db48f841cf04c6b54f494f18dcfb
Sally Diament <sally.barzilai@gmail.com>
2023-03-15 22:54:18 -0300

    incluindo req 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page = selector.css('a.next.page-numbers::attr(href)').get()
+    return next_page
 
 
 # Requisito 4
