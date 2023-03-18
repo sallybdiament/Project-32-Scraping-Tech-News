77bbdcede27af5cfb8a0c8b05e156924df279225
Sally Diament <sally.barzilai@gmail.com>
2023-03-18 17:53:56 -0300

    corrigindo req 8 q tava passando data ao inved de title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,5 @@
 from tech_news.database import search_news
-# import re
+import re
 from datetime import datetime
 
 
@@ -15,13 +15,13 @@ def search_by_title(title):
 # Requisito 8
 def search_by_date(date):
     try:
-        # mat = re.match('(\\d{2})[/.-](\\d{2})[/.-](\\d{4})$', date)
-        # if mat is not None:
-        tuples = []
-        d = datetime.strptime(date, "%d/%m/%Y").date().strftime('%Y-%m-%d')
-        results = search_news({"timestamp": {"$regex": d}})
-        for result in results:
-            tuples.append((result["timestamp"], result["url"]))
+        mat = re.match('(\\d{2})[/.-](\\d{2})[/.-](\\d{4})$', date)
+        if mat is not None:
+            tuples = []
+            d = datetime.strptime(date, "%d/%m/%Y").date().strftime('%Y-%m-%d')
+            results = search_news({"timestamp": {"$regex": d}})
+            for result in results:
+                tuples.append((result["title"], result["url"]))
         return tuples
     except ValueError:
         raise ValueError("Data inválida")
